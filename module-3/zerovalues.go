f8b11a2703f4e2132287f15960cb32826dd1f815
benirios <rbenirios09@gmail.com>
2024-07-09 11:59:19 +0100

    module-3

---
+++ module-3/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Every type of value in go have its own default value called zero value
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("Int: %v\n", i)
+	fmt.Printf("float: %v\n", f)
+	fmt.Printf("bool: %v\n", b)
+	fmt.Printf("string: %q\n", s)
+	// ------------------------------------------------------
+
+}
